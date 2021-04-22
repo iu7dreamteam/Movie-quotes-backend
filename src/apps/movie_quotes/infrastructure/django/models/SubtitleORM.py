d4e802e7f64bb8da009013784d024fd6d5d8763e
Rostislav V <sunz89@yandex.ru>
2021-04-22 13:26:38 +0300

    Fix django-config for considering paths to apps, models, migrations in different folders

--- src/apps/movie_quotes/infrastructure/django/models/SubtitleORM.py
+++ src/apps/movie_quotes/infrastructure/django/models/SubtitleORM.py
@@ -4,6 +4,9 @@ from django.contrib.postgres.indexes import GinIndex
 from apps.movie_quotes.infrastructure.django.models.MovieORM import MovieORM
 
 class SubtitleORM(models.Model):
+    class Meta:
+        app_label = 'movie_quotes'
+
     quote = models.CharField(max_length = 500, db_index = True)
     start_time = models.TimeField()
     end_time = models.TimeField()
