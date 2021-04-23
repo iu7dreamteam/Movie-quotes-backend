f397eb5a907fef5553cbf1df86ac59307f4164a9
AnaNek <neknas28@gmail.com>
2021-04-23 23:53:08 +0300

    Add fields about direcor, poster url and video url in movie model

--- src/apps/movie_quotes/domain/entities/Movie.py
+++ src/apps/movie_quotes/domain/entities/Movie.py
@@ -1,38 +1,56 @@
 class Movie(object):
-    def __init__(self, id = None, name = '', year = 0, url = ''):
+    def __init__(self, id = None, title = '', year = 0, director = '', poster_url = '', video_url = ''):
         self._id = id
-        self._name = name
+        self._title = title
         self._year = year
-        self._url = url
+        self._director = director
+        self._poster_url = poster_url
+        self._video_url = video_url
 
     @property
     def id(self):
         return self._id
 
     @property
-    def name(self):
-        return self._name
+    def title(self):
+        return self._title
 
     @property
     def year(self):
         return self._year
 
     @property
-    def url(self):
-        return self._url
+    def director(self):
+        return self._director
+
+    @property
+    def poster_url(self):
+        return self._poster_url
+
+    @property
+    def video_url(self):
+        return self._video_url
 
     @id.setter
     def id(self, id):
         self._id = id
 
-    @name.setter
-    def name(self, name):
-        self._name = name
+    @title.setter
+    def name(self, title):
+        self._title = title
 
     @year.setter
     def year(self, year):
         self._year = year
 
-    @url.setter
-    def url(self, url):
-        self._url = url
+    @director.setter
+    def director(self, director):
+        self._director = director
+
+    @poster_url.setter
+    def poster_url(self, poster_url):
+        self._poster_url = poster_url
+
+    @video_url.setter
+    def video_url(self, video_url):
+        self._video_url = video_url
