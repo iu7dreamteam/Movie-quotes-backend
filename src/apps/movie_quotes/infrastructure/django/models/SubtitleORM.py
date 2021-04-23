b03ebd3f03086112cb386389241598ccb338e5bd
AnaNek <neknas28@gmail.com>
2021-04-23 21:50:03 +0300

    Update relation between subtitle and movie to one-to-many

--- src/apps/movie_quotes/infrastructure/django/models/SubtitleORM.py
+++ src/apps/movie_quotes/infrastructure/django/models/SubtitleORM.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.postgres.search import SearchVectorField
-from django.contrib.postgres.indexes import GinIndex
 from apps.movie_quotes.infrastructure.django.models.MovieORM import MovieORM
 
 class SubtitleORM(models.Model):
@@ -10,4 +8,4 @@ class SubtitleORM(models.Model):
     quote = models.CharField(max_length = 500, db_index = True)
     start_time = models.TimeField()
     end_time = models.TimeField()
-    movies = models.ManyToManyField(MovieORM)
+    movie = models.ForeignKey(MovieORM, on_delete=models.CASCADE)
