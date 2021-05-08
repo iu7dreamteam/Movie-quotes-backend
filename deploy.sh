5b1b8c368aae79e06c3bb48e5822e6dbddfcd5c3
rflban <nabiev.alsn@yandex.ru>
2021-05-08 22:09:21 +0300

    MQ-52: fix deploy.sh 'the input device is not a TTY' issue

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker-compose stop
 docker-compose rm -f
 docker pull sansanchezzz/movie-quotes:latest
 docker-compose up -d --force-recreate
-docker-compose exec web python3 manage.py migrate
+docker-compose exec -T web python3 manage.py migrate
