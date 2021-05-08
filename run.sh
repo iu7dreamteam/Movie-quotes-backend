2413ac81b01eb9b903484ab534516a25c5cc4bdf
rflban <nabiev.alsn@yandex.ru>
2021-05-08 21:46:15 +0300

    MQ-52: add deploy script to CD

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker pull sansanchezzz/movie-quotes:latest
-docker rm -f $(docker ps -aq)
-cd backend
-docker-compose up -d --force-recreate
-docker-compose exec web python3 manage.py migrate
